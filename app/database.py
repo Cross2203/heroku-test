53f1c1c784843ca5d1129e71d4a9d804e9cf3457
Cross2203 <ciae1996@gmail.com>
2024-06-15 18:36:48 -0500

    test

--- app/database.py
+++ app/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost/test"
+SQLALCHEMY_DATABASE_URL = "postgresql://test_ad7y_user:dpg-cpn28tuehbks7380omkg-a.oregon-postgres.render.com/test_ad7y"
+
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
