423fbdcf4e811cd372f3c4bdf5120d0c6d8744c4
Cross2203 <ciae1996@gmail.com>
2024-06-15 18:40:38 -0500

    sql

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://test_ad7y_user:dpg-cpn28tuehbks7380omkg-a.oregon-postgres.render.com/test_ad7y"
+SQLALCHEMY_DATABASE_URL = "postgresql://test_ad7y_user:htxkY2k0BXvMNGAqFYcoY5PrCq6E1pyi@dpg-cpn28tuehbks7380omkg-a.oregon-postgres.render.com/test_ad7y"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
